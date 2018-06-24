9a3c5a17777794507079c109e359d5233d93ad5d
Roman <IamFilter94@gmail.com>
2018-06-24 12:01:30 +0600

    Code refinenements

--- src/main/scala/observatory/Interaction.scala
+++ src/main/scala/observatory/Interaction.scala
@@ -3,7 +3,7 @@ package observatory
 import com.sksamuel.scrimage.Image
 import observatory.helpers.par.ParInteractor
 
-import math._
+import math.{pow, _}
 
 /**
   * 3rd milestone: interactive visualization
@@ -13,12 +13,7 @@ object Interaction {
     * @param tile Tile coordinates
     * @return The latitude and longitude of the top-left corner of the tile, as per http://wiki.openstreetmap.org/wiki/Slippy_map_tilenames
     */
-  def tileLocation(tile: Tile): Location = {
-    val n = pow(2.0, tile.zoom)
-    val lon = tile.x / n * 360.0 - 180.0
-    val lat = toDegrees(atan(sinh(Pi * (1 - (2 * tile.y / n)))))
-    Location(lat, lon)
-  }
+  def tileLocation(tile: Tile): Location = tile.location
 
   def tile(temperatures: Iterable[(Location, Temperature)], colors: Iterable[(Temperature, Color)], tile: Tile): Image = {
     ParInteractor.tile(temperatures, colors, tile)
@@ -37,8 +32,9 @@ object Interaction {
     for {
       (year, data) <- yearlyData.par
       zoomLevel <- Range(0, 4).par
-      tileX <- Range(0, pow(2, zoomLevel).toInt).par
-      tileY <- Range(0, pow(2, zoomLevel).toInt).par
+      subtiles = pow(2, zoomLevel).toInt
+      tileX <- Range(0, subtiles).par
+      tileY <- Range(0, subtiles).par
     } {
 //      println(s"Processing year $year, zoom level: $zoomLevel, x: $tileX, y: $tileY")
       val tile = Tile(tileX, tileY, zoomLevel)
